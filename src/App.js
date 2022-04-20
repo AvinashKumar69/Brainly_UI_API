012e2f7d29390ec044cb63965ce82f35e45211a1
Avinash Kumar <ak2449746@gmail.com>
2022-04-20 17:36:36 +0530

    Update... added Routes...

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './components/Footer';
+import NavBar from './components/NavBar';
+import AskQuestion from './pages/AskQuestion';
 import Home from './pages/Home';
+import JoinNowPage from './pages/JoinNowPage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <>
-      <Home />
+      <NavBar />
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/loginpage' element={<LoginPage />} />
+        <Route exact path='/joinnowpage' element={<JoinNowPage />} />
+        <Route exact path='/askquestion' element={<AskQuestion />} />
+      </Routes>
+      <Footer />
     </>
   );
 }
---
+++ src/pages/JoinNowPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const JoinNowPage = () => {
+    return (
+        <div>JoinNowPage</div>
+    )
+}
+
+export default JoinNowPage;
